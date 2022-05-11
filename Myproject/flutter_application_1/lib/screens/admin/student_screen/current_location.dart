9ca08038e44a73d4a0cafe6c2d370f506a9ec983
Ayoub Elidrissi <el.idrissi.benme@gmail.com>
2022-05-11 18:50:59 +0200

    demo simple map

--- Myproject/flutter_application_1/lib/screens/admin/student_screen/current_location.dart
+++ Myproject/flutter_application_1/lib/screens/admin/student_screen/current_location.dart
@@ -1,20 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CurrentLocationScreen extends StatefulWidget {
-  final DocumentSnapshot post;
+  const CurrentLocationScreen({Key? key, DocumentSnapshot<Object?>? post})
+      : super(key: key);
 
   // ignore: use_key_in_widget_constructors
-  const CurrentLocationScreen({required this.post});
+  //const CurrentLocationScreen({required this.post});
   @override
   State<CurrentLocationScreen> createState() => _CurrentLocationScreenState();
 }
 
 class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
   late GoogleMapController googleMapController;
+
   static const CameraPosition initialCameraPosition = CameraPosition(
-      target: LatLng(37.42796133586664, -122.085749655962), zoom: 14);
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14);
   Set<Marker> markers = {};
 
   @override
@@ -22,6 +25,7 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Huidige Locatie"),
+        centerTitle: true,
       ),
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
@@ -32,6 +36,55 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
           googleMapController = controller;
         },
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          Position position = await _determinePosition();
+
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                  zoom: 14)));
+
+          markers.clear();
+
+          markers.add(Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: LatLng(position.latitude, position.longitude)));
+
+          setState(() {});
+        },
+        label: const Text("Huidige Locatie"),
+        icon: const Icon(Icons.location_history),
+      ),
     );
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
   }
 }
